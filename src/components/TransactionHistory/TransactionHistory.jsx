d07b18ff90606d709a96435a4fb610014ae23a1f
Yavorskiy Vladislav <vl.yavorskiy01@gmail.com>
2024-08-14 14:57:54 +0300

    Update

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -5,9 +5,9 @@ export default function TransactionHistory({ items }) {
     <table className={css.tableContainer}>
       <thead>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.thCell}>Type</th>
+          <th className={css.thCell}>Amount</th>
+          <th className={css.thCell}>Currency</th>
         </tr>
       </thead>
 
@@ -15,9 +15,9 @@ export default function TransactionHistory({ items }) {
         {items.map(({ id, type, amount, currency }) => {
           return (
             <tr key={id}>
-              <td>{type}</td>
-              <td>{amount}</td>
-              <td>{currency}</td>
+              <td className={css.tdCell}>{type}</td>
+              <td className={css.tdCell}>{amount}</td>
+              <td className={css.tdCell}>{currency}</td>
             </tr>
           );
         })}
